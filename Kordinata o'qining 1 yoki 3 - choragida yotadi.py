f52b58ce7898d159efd36b9de8e2ebe8b9752bb1
TEMURBEK RUSTAMOV ERGASHBOY UGLI <128171357+TEMURBEKRUSTAMOV@users.noreply.github.com>
2023-12-10 00:28:28 +0500

    Add files via upload

---
+++ Kordinata o'qining 1 yoki 3 - choragida yotadi.py
@@ -0,0 +1,11 @@
+x=int(input('x='))
+y=int(input('y='))
+if x>0 and y>0:
+    print('Kordinata oqining II-choragida yotadi')
+else :
+    print('Kordinata oqining II-choragida yotmaydi')
+if x<0 and y<0:
+    print('Kordinata oqining III-choragida yotadi')
+else :
+    print('Kordinata oqining III-choragida yotmaydi')
+    
