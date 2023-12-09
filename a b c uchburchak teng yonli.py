8313ead1ff67967411941e5e81387338dfedd78e
TEMURBEK RUSTAMOV ERGASHBOY UGLI <128171357+TEMURBEKRUSTAMOV@users.noreply.github.com>
2023-12-10 00:29:08 +0500

    Add files via upload

---
+++ a b c uchburchak teng yonli.py
@@ -0,0 +1,12 @@
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+if a==b!=c:
+    print('Teng tomonli uchburchak')
+if a==c!=b:
+    print('Teng tomonli uchburchak')
+if b==c!=a:
+    print('Teng tomonli uchburchak')
+else :
+    print('Teng tomonli uchburchak emas')
+
