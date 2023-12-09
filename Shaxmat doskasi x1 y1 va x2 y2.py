46d0932250472e670c50e3063b7f68038c6dcc10
TEMURBEK RUSTAMOV ERGASHBOY UGLI <128171357+TEMURBEKRUSTAMOV@users.noreply.github.com>
2023-12-10 00:30:00 +0500

    Add files via upload

---
+++ Shaxmat doskasi x1 y1 va x2 y2.py
@@ -0,0 +1,10 @@
+print('Shaxmat doskasidan x y ')
+x=int(input('x1='))
+y=int(input('y1='))
+x1=int(input('x2='))
+y1=int(input('y2='))
+if 1<x<8 and 1<y<8:
+    if 1<x1<8 and 1<y1<8:
+        print('Oq maydonda')
+else :
+    print('Qora maydon')
